f05a18bca3990e8ad51989366830569c554a82b3
camily <camilyzipora08@gmail.com>
2026-05-23 20:09:49 -0300

    base, index e sobre

--- app/views.py
+++ app/views.py
@@ -2,3 +2,28 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+def sobre(request):
+    return render(request, 'sobre.html')
+
+
+def elenco(request):
+
+    personagens = [
+        {
+            'nome': 'Sophie Hatter',
+            'dublador': 'Patrícia Scalvi',
+            'imagem': 'imgs/sophie.jpeg'
+        },
+
+        {
+            'nome': 'Howl',
+            'dublador': 'Marcelo Campos',
+            'imagem': 'imgs/howl.jpeg'
+        },
+    ]
+
+    return render(request, 'elenco.html', {
+        'personagens': personagens
+    })
+def elenco(request):
+    return render(request, 'elenco.html')
